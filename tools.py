3295278e0d00e2972f6771d21fd8a6e576153728
Vultrao <thomas.lourseyre@gmail.com>
2019-04-27 23:08:07 +0200

    Premiere version fonctionnelle

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
 import sys
+from pprint import pprint
 
 def eprint(string_message):
     sys.stderr.write("[ERROR] "+__file__+": "+string_message)
@@ -9,3 +10,7 @@ def eprint(string_message):
 def dprint(string_message, DEBUG_MODE):
     if DEBUG_MODE:
         print("[DEBUG] "+__file__+": "+string_message)
+
+def dump(var, DEBUG_MODE):
+    if DEBUG_MODE:
+        pprint(var)
--- tracking_request.py
+++ tracking_request.py
@@ -8,17 +8,13 @@ import tools
 def tracking_request(string_from, string_to, DEBUG_MODE=False):
     gmaps = googlemaps.Client(key='AIzaSyDUxIxbbDx9ZMv9SzuVYfenr12APcJ9xg4')
 
-    # Getting the coordinates
-    #geocode_from = gmaps.geocode(string_from)
-    #geocode_to = gmaps.geocode(string_to)
-
     # Putting them into the calculator
-    now = datetime.now()
-    #directions_result = gmaps.directions(geocode_from, geocode_to, mode="transit", departure_time=now)
     directions_result = gmaps.directions(string_from, string_to)
 
+    eta = directions_result[0]["legs"][0]["duration"]["text"]
+
     # debug trace
-    tools.dprint(directions_result, DEBUG_MODE);
+    tools.dprint(eta, DEBUG_MODE)
 
     # Outputting the result
-    return
+    return eta
