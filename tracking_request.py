6abe28f223f3eacdb9bbf0ed7fe6b2d77e84cd3b
Vultrao <thomas.lourseyre@gmail.com>
2019-05-03 14:43:54 +0200

    Modification du workspace et changement de la cle API

--- tracking_request.py
+++ tracking_request.py
@@ -6,7 +6,7 @@ import tools
 
 # Arguments must be strings
 def tracking_request(string_from, string_to, DEBUG_MODE=False):
-    gmaps = googlemaps.Client(key='AIzaSyDUxIxbbDx9ZMv9SzuVYfenr12APcJ9xg4')
+    gmaps = googlemaps.Client(key='API-ID-googlemaps')
 
     tools.dprint("from:"+string_from+" to:"+string_to, DEBUG_MODE)
 
